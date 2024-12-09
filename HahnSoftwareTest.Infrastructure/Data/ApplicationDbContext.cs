a16eae9938194afe214632c723906edda2324a23
Rafael Cavalcanti <rcavalcanti@lenovo.com>
2024-12-09 15:32:09 -0300

    add: create ApplicationDbContext class with DbSets for entities

---
+++ HahnSoftwareTest.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HahnSoftwareTest.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
